9220e95d4cc476987eea88a0c2fea1f2c3e32919
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-05-02 17:00:10 -0700

    print while iterations

--- no.intermediates.selected.R
+++ no.intermediates.selected.R
@@ -1,5 +1,19 @@
+library(data.table)
 library(penaltyLearning)
-N <- 1000000
+N <- 10
 dt <- data.table(loss.seq=(N-1):0, complexity=0:(N-1))
 dt[, loss := ifelse(complexity %in% c(0, N-1), loss.seq, loss.seq+0.5)]
 modelSelection(dt)
+
+## requires while-iteration branch.
+result <- .C(
+  "modelSelectionFwd_interface",
+  loss=as.double(dt$loss),
+  complexity=as.double(dt$complexity),
+  N=as.integer(nrow(dt)),
+  models=integer(nrow(dt)),
+  breaks=double(nrow(dt)),
+  evals=integer(nrow(dt)),
+  PACKAGE="penaltyLearning")
+print(result)
+sum(result$evals)
