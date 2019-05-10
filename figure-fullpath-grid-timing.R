318daa017ab1c74c450d2599e1a280a3cde0aa0e
Toby Dylan Hocking <tdhock5@gmail.com>
2019-05-10 16:04:59 -0700

    both figu

--- figure-fullpath-grid-timing.R
+++ figure-fullpath-grid-timing.R
@@ -1,7 +1,7 @@
 source("packages.R")
 
-db.times <- readRDS("fullpath.db.timing.rds")
-big.times <- db.times[N==max(N)]
+fullpath.db.timing <- readRDS("fullpath.db.timing.rds")
+big.times <- fullpath.db.timing[N==max(N)]
 big.times[, seconds := time/1e9]
 big.stats <- big.times[, list(
   mean=mean(seconds),
@@ -47,12 +47,105 @@ gfac <- function(x){
 }
 timing.dt[, grid.fac := gfac(n.grid)]
 big.stats[, grid.fac := gfac("Exact")]
+big.stats[, n.grid := Inf]
 both.dt <- rbind(
-  timing.dt[, .(mean=mean.seconds, sd=sd.seconds, algorithm, grid.fac)],
-  big.stats[, .(mean, sd, algorithm, grid.fac)])
+  timing.dt[, .(
+    mean=mean.seconds, sd=sd.seconds, algorithm, grid.fac, n.grid)],
+  big.stats[, .(mean, sd, algorithm, grid.fac, n.grid)])
 text.dt <- both.dt[, .SD[which.max(grid.fac)], by=list(algorithm)]
+
+
+## from other file.
+fullpath.db.timing[, seconds := time/1e9]
+timing.stats <- fullpath.db.timing[, list(
+  mean=mean(seconds),
+  sd=sd(seconds)
+), by=list(N, expr)]
+timing.stats[, mean.minutes := mean/60]
+timing.stats[, mean.hours := mean.minutes/60]
+print(timing.stats)
+ref.dt <- data.table(
+  seconds=c(1, 60),
+  label=paste(" 1", c("second", "minute")))
+max.dt <- timing.stats[N==1e5]
+algo.colors <- c(
+  linear="black",
+  quadratic="blue",
+  Exact_linear="black",
+  Exact_quadratic="blue",
+  Approx_grid="red")
+models.var <- "N = number of input models to select
+(exact algorithms only)"
+grid.var <- "Grid points used in approximate computation"
+both.dt[, xvar := grid.var]
+text.dt[, xvar := grid.var]
+timing.stats[, xvar := models.var]
+max.dt[, xvar := models.var]
 gg <- ggplot()+
-  xlab("Grid points used in approximate computation")+
+  facet_grid(. ~ xvar, scales="free")+
+  geom_hline(aes(
+    yintercept=seconds),
+    data=ref.dt,
+    color="grey")+
+  scale_color_manual(values=algo.colors)+
+  scale_fill_manual(values=algo.colors)+
+  geom_text(aes(
+    1e1, seconds, label=label),
+    data=data.table(ref.dt, xvar=models.var),
+    color="grey",
+    hjust=0,
+    vjust=-0.5)+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "lines"))+
+  geom_line(aes(
+    N, mean, color=expr),
+    data=timing.stats)+
+  geom_text(aes(
+    N, mean, color=expr, label=paste0(ifelse(
+      expr=="linear",
+      paste(round(mean*1e3), "ms"),
+      paste(round(mean.minutes), "minutes")), "  ")),
+    hjust=1,
+    vjust=0,
+    data=max.dt)+
+  geom_point(aes(
+    N, mean, color=expr),
+    data=max.dt)+
+  geom_ribbon(aes(
+    N, ymin=mean-sd, ymax=mean+sd, fill=expr),
+    alpha=0.5,
+    data=timing.stats)+
+  geom_segment(aes(
+    n.grid, mean-sd,
+    xend=n.grid, yend=mean+sd,
+    color=algorithm),
+    data=both.dt)+
+  geom_point(aes(
+    n.grid, mean,
+    color=algorithm),
+    shape=1,
+    data=both.dt)+
+  geom_blank(aes(
+    x, y),
+    data=data.table(xvar=models.var, x=5e6, y=1))+
+  geom_text(aes(
+    n.grid, mean,
+    color=algorithm,
+    label=paste0(algorithm, "  ")),
+    hjust=1,
+    vjust=0.5,
+    data=text.dt)+
+  scale_x_log10("")+
+  guides(fill="none", color="none")+
+  scale_y_log10("Time to compute model
+selection function (seconds)")
+(dl <- directlabels::direct.label(gg, "last.polygons"))
+png("figure-fullpath-both-timing.png", 6, 2.5, units="in", res=300)
+print(dl)
+dev.off()
+
+gg <- ggplot()+
+  xlab(grid.var)+
   scale_y_log10(
     "Seconds to compute model selection function",
     breaks=br.vec,
